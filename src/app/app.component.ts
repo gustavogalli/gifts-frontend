cb9217c57d4b4243bea02015b40e96170524e80e
gustavogalli <gustavosgalli@gmail.com>
2025-05-01 18:36:24 -0300

    [feat] Updating App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gifts-frontend';
+export class AppComponent implements OnInit {
+  isAuthPage: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isAuthPage = event.url.includes('/login') || event.url.includes('/register');
+      }
+    });
+  }
 }
