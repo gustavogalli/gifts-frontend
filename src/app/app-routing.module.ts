7a52cedb315d4647ef8ee356f7b9512cad62514e
gustavogalli <gustavosgalli@gmail.com>
2025-05-01 18:36:51 -0300

    [feat] Updating Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProductsListComponent } from './products/products-list/products-list.component';
+import { CartComponent } from './products/cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsListComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'category/:category', component: CategoryComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
 ];
 
 @NgModule({
