2c3706b39a956cf28b5d12acb53aea66e98033ff
gustavogalli <gustavosgalli@gmail.com>
2025-05-01 18:35:09 -0300

    [feat] Adding Product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../model/Product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = 'http://localhost:8080/products';
+
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+    return this.http.get<Product[]>(this.apiUrl, { headers });
+  }
+
+  gettAllCategories(): Observable<string[]> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+    return this.http.get<string[]>(this.apiUrl + "/category", { headers });
+  }
+
+  getProductsByCategory(category: string): Observable<Product[]> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  
+    return this.http.get<Product[]>(`${this.apiUrl}/category/${category}`, { headers });
+  }
+
+  purchaseProducts(products: Product[]): Observable<void> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  
+    return this.http.post<void>(`${this.apiUrl}/purchase`, products, { headers });
+  }
+  
+}
