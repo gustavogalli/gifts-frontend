64b868a5725caced7bf4dba645e54ed1a9a54ee4
gustavogalli <gustavosgalli@gmail.com>
2025-05-01 18:12:55 -0300

    [feat] Adding navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  categories: string[] = [];
+
+  constructor(private router: Router, private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productService.gettAllCategories().subscribe(
+      data => {
+        this.categories = data.sort((a, b) => a.localeCompare(b));
+      },
+      err => console.error('Failed to get categories:', err)
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
